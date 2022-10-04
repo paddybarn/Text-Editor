04d958678052366781206b34e24f12c3e9af0198
paddybarn <107075686+paddybarn@users.noreply.github.com>
2022-10-04 15:12:48 -0400

    fixed service worker

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -37,12 +37,12 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result ?.value;
 }
 console.error('getDb not implemented');
 
